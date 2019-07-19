e8f5e7c29fde6b3f40fdeb9bfccac49e7d5726b9
Tetiana Hrytsaienko <kortatiana89@gmail.com>
2019-07-19 09:40:08 +0300

    Updates

---
+++ precache-manifest.16a1954ca84110cc2fbc65d5258a8337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11985e2aa166abeb3860e488ea06ae12",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "00e25cbd0f9879e7ad8f",
+    "url": "/monsters/static/css/main.f06f77a1.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "00e25cbd0f9879e7ad8f",
+    "url": "/monsters/static/js/main.7f7f2a05.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
